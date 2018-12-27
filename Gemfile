3657da4849ef299f1b62dc19a047895bd441411f
Message_Me <brionholland2000@gmail.com>
2018-12-27 22:36:34 +0000

    seeded database and created users model

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'semantic-ui-sass'
 gem 'jquery-rails'
 gem 'jquery'
+gem 'bcrypt'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "brandon", password: "password")
+User.create(username: "amanda", password: "password")
+User.create(username: "william", password: "password")
+User.create(username: "victoria", password: "password")
+User.create(username: "brion", password: "password")
